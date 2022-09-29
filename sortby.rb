1531b7e58f4b850d45bbea36b9727e8417a11658
prateek2tu <prateekkhadson15@gmail.com>
2022-09-29 16:28:36 +0530

    quick sort

---
+++ sortby.rb
@@ -0,0 +1,9 @@
+def quick_sort(list)
+  return [] if list.empty?
+  groups = list.group_by { |n| n <=> list.first }
+  less_than    = groups[-1] || []
+  first        = groups[0]  || []
+  greater_than = groups[1]  || []
+  quick_sort(less_than) + first + quick_sort(greater_than)
+end
+p quick_sort [3, 7, 2, 1, 8, 12]
